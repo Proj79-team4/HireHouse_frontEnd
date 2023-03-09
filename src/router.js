7415c049a209a92b1a87718b8de462146157b172
ManganiBiagio <biagiomangani@live.it>
2023-03-09 10:11:27 +0100

    fix error route

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import HomePage from "./pages/Home.vue";
-import PostsShowPage from "./pages/posts/Show.vue";
+
 // ... import di altri componenti
 
 const router = createRouter({
