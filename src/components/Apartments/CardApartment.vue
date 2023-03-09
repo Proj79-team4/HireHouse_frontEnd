5b91a5ff91a13fa18e71f9fa54f0ca9b3f39ffab
Sofia-Citrini <sofiacitrini@gmail.com>
2023-03-09 18:18:06 +0100

    finished homepage

---
+++ src/components/Apartments/CardApartment.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card rounded-4">
+        <div class="row no-gutters">
+            <div class="col-4">
+                <img src="/Milano.jpg" class="img-fluid h-100 rounded-4" alt="">
+            </div>
+            <div class="col">
+                <div class="card-block px-2">
+                    <h4 class="card-title">Title</h4>
+                    <p class="card-text">Description</p>
+                    <a href="#" class="btn btn-primary">BUTTON</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script></script>
+
+<style scoped lang="scss">
+    .card{
+        border: none
+    }
+</style>
---
+++ src/pages/Apartments/Index.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="py-3">
+        <div class="container d-flex justify-content-between align-items-center">
+            <div>
+                <a href=""><img src="/Logo-bianco.png" width="200"></a>
+            </div>
+
+            <div class="pt-2">
+                <a href="" class="nav-link">
+                    Torna alla Home
+                    <i class="fa-solid fa-house ms-3"></i>
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <TheSearchbar></TheSearchbar>
+
+    <!-- lista appartamenti -->
+    <section class="container">
+        <div class="row">
+            <div class="col-8">
+                <CardApartment></CardApartment>
+            </div>
+            <div class="col-4"></div>
+        </div>
+    </section>
+</template>
+
+<script>
+import TheSearchbar from "../../components/Apartments/TheSearchbar.vue";
+import CardApartment from '../../components/Apartments/CardApartment.vue';
+
+export default {
+    name: 'ApartmentIndex',
+    components: { TheSearchbar, CardApartment }
+}
+</script>
