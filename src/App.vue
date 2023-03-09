e4f26b139359d326eb9c62766de1056b220a3802
Sofia-Citrini <sofiacitrini@gmail.com>
2023-03-09 11:36:33 +0100

    created navbar and searchbar

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,23 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheNavbarHome from './components/TheNavbarHome.vue';
+import TheHeaderHome from './components/TheHeaderHome.vue'
 
 export default{
   components:{
-    HelloWorld
+    TheNavbarHome,
+    TheHeaderHome
   }
 }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <TheNavbarHome></TheNavbarHome>
+  <TheHeaderHome></TheHeaderHome>
 
   <router-view></router-view>
 </template>
 
 <style lang="scss">
-
 @use "./styles/general.scss";
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/TheHeaderHome.vue
@@ -0,0 +1,42 @@
+<template>
+    <section class="bg-black py-3">
+        <div class="container">
+            <!-- searchbar -->
+            <div class="row">
+                <div class="col-12 d-flex justify-content-center">
+                    <div class="input-group mb-3 searchbar px-3 py-2 rounded-5 align-items-center">
+                        <div class="d-flex flex-column flex-fill px-2">
+                            <h6 class="mb-0"><small>Localitá</small></h6>
+                            <input type="text" class="form-control input-text" placeholder="Inserisci la tua prossima meta" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                        </div>
+
+                        <div class="input-group-append ps-2">
+                            <button class="btn my-btn-orange rounded-circle" type="button"><i class="fa fa-search"></i></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script></script>
+
+<style scoped lang="scss">
+@use '../styles/partiarls/variables';
+.searchbar {
+    width: 70%;
+    background-color: white;
+
+    .form-control{
+        border: 0;
+        padding: 0;
+
+        &:focus {
+            border-color: transparent; 
+            outline: 0;
+            box-shadow: 0 0 0 0 white !important;
+        }
+    }
+}
+</style>
