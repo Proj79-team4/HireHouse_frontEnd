63c07da03ec478ccbd4c11b1b852c08234e391a6
ManganiBiagio <biagiomangani@live.it>
2023-03-15 16:59:48 +0100

    add form message

--- src/pages/Apartments/Show.vue
+++ src/pages/Apartments/Show.vue
@@ -12,19 +12,21 @@
             </div>
         </section>
 
-        <div class="row "  v-if="apartment != null">
+        <div class="row " v-if="apartment != null">
             <!-- informazioni appartamento -->
             <div class="col-12 col-lg-8 pe-5">
                 <section>
                     <div class="row border-bottom pb-3">
                         <div class="col-9">
-                            <h4>Host: {{ apartment.user.name +" "+ apartment.user.surname }}</h4>
-                            <div class="fw-bold"><small>{{ apartment.num_rooms }} Stanze · {{ apartment.num_beds }} Letti · {{
-                                apartment.num_bathrooms }} Bagni · {{ apartment.square_meters }} m²</small></div>
+                            <h4>Host: {{ apartment.user.name + " " + apartment.user.surname }}</h4>
+                            <div class="fw-bold"><small>{{ apartment.num_rooms }} Stanze · {{ apartment.num_beds }} Letti ·
+                                    {{
+                                        apartment.num_bathrooms }} Bagni · {{ apartment.square_meters }} m²</small></div>
                         </div>
                         <div class="col-3 d-flex justify-content-end">
                             <div class="host-img">
-                                <img :src="'http://127.0.0.1:8000/' + 'storage/' + apartment.user.profile_image" class="img-fluid" alt="">
+                                <img :src="'http://127.0.0.1:8000/' + 'storage/' + apartment.user.profile_image"
+                                    class="img-fluid" alt="">
                             </div>
                         </div>
                     </div>
@@ -39,8 +41,8 @@
                             <!-- servizi -->
                             <div class="py-3">
                                 <h5 class="py-3">Servizi appartamento</h5>
-                                <div v-for="(service,i) in apartment.services" class="py-1 px-4">
-                                    <i :class="'fa-solid ' + service.icon  "></i>
+                                <div v-for="(service, i) in apartment.services" class="py-1 px-4">
+                                    <i :class="'fa-solid ' + service.icon"></i>
                                     <span class="ps-3">{{ service.name }}</span>
                                 </div>
                             </div>
@@ -49,8 +51,8 @@
                             <!-- Regole -->
                             <div class="py-3">
                                 <h5 class="py-3">Regole appartamento</h5>
-                                <div v-for="(rule,i) in apartment.rules" class="py-1 px-4">
-                                    <i :class="'fa-solid ' + rule.icon  "></i>
+                                <div v-for="(rule, i) in apartment.rules" class="py-1 px-4">
+                                    <i :class="'fa-solid ' + rule.icon"></i>
                                     <span class="ps-3">{{ rule.name }}</span>
                                 </div>
                             </div>
@@ -72,21 +74,31 @@
             <div class="col-12 col-lg-4 py-4">
                 <div class="shadow p-3 rounded-4">
                     <h5 class="text-center py-3">€{{ apartment.price }} /notte</h5>
-                    <form action="" method="">
+                    <form action="" method="" @submit.prevent="createMessage()">
                         <div class="pt-2 px-3 border rounded-top">
                             <label for="exampleFormControlInput1" class="fw-bold"><small>NOME</small></label>
-                            <input type="text" class="form-control form-control-sm" id="exampleFormControlInput1" placeholder="Inserisci il tuo nome">
+                            <input type="text" class="form-control form-control-sm" id="exampleFormControlInput1"
+                                placeholder="Inserisci il tuo nome" v-model="formMessage.name" required>
                         </div>
                         <div class="pt-2 px-3 border border-top-0">
                             <label for="exampleFormControlInput1" class="fw-bold"><small>E-MAIL</small></label>
-                            <input type="email" class="form-control form-control-sm" id="exampleFormControlInput1" placeholder="Inserisci la tua email">
+                            <input type="email" class="form-control form-control-sm" id="exampleFormControlInput1"
+                                placeholder="Inserisci la tua email" v-model="formMessage.email" required>
                         </div>
                         <div class="pt-2 px-3 border border-top-0 rounded-bottom mb-2">
-                            <label for="exampleFormControlTextarea1" class="fw-bold"><small>INSERISCI MESSAGGIO</small></label>
-                            <textarea class="form-control form-control-sm" id="exampleFormControlTextarea1" rows="3" placeholder="Scrivi qui"></textarea>
+                            <label for="exampleFormControlTextarea1" class="fw-bold"><small>INSERISCI
+                                    MESSAGGIO</small></label>
+                            <textarea class="form-control form-control-sm" id="exampleFormControlTextarea1" rows="3"
+                                placeholder="Scrivi qui" v-model="formMessage.content" required></textarea>
                         </div>
 
-                        <a href="" class="btn my-btn-orange d-block">Invia</a>
+
+                        <div class="d-flex align-items-center gap-2">
+                            <button class="btn my-btn-orange d-block" type="submit">Invia</button>
+                            <span class="text-success" v-if="formSubmit === true" >Messaggio inviato correttamente</span>
+
+                        </div>
+
                     </form>
                 </div>
             </div>
@@ -100,26 +112,63 @@
 import TheNavbar from '../../components/Apartments/TheNavbar.vue';
 import TheFooter from '../../components/TheFooter.vue';
 import TheMapMarker from "../../components/TheMapMarker.vue"
-import axios from "axios"; 
+import axios from "axios";
 
 export default {
     name: 'ApartmentShow',
-    components: { TheNavbar, TheFooter,TheMapMarker },
+    components: { TheNavbar, TheFooter, TheMapMarker },
     data() {
         return {
-            apartment: null
+            formSubmit: false,
+            apartment: null,
+            formMessage: {
+                name: "",
+                email: "",
+                content: "",
+                apartment_id: null
+            }
         }
     },
-    methods:{
-        fetchIndexApartments(){
-            axios.get("http://127.0.0.1:8000/api/apartments/show/"+this.$route.params.id)
-            .then((resp)=>{
-                this.apartment=resp.data
-            })
+    methods: {
+        fetchIndexApartments() {
+            axios.get("http://127.0.0.1:8000/api/apartments/show/" + this.$route.params.id)
+                .then((resp) => {
+                    this.apartment = resp.data
+                })
+
+        },
+        createMessage() {
+
+
+            const formData = new FormData();
+            formData.append("name", this.formMessage.name);
+            formData.append("email", this.formMessage.email);
+            formData.append("content", this.formMessage.content);
+            formData.append("apartment_id", this.apartment.id);
+
+            axios.post("http://127.0.0.1:8000/api/message/create", formData)
+                .then((resp) => {
+                    this.formSubmit = true;
+                    this.formMessage = {
+                        name: "",
+                        email: "",
+                        content: "",
+                        apartment_id: null
+
+                    }
+                    setTimeout(()=>{ this.formSubmit=false }, 2000);
+
+
+
+
+
+                }).catch((e) => {
+
+                })
 
         }
     },
-    created(){
+    created() {
         this.fetchIndexApartments();
     }
 
@@ -128,6 +177,7 @@ export default {
 
 <style scoped lang="scss">
 @use '../../styles/partials/variables';
+
 .apartment-image {
     height: 60vh;
     padding: 2rem 0;
@@ -155,7 +205,7 @@ export default {
 }
 
 form {
-    .form-control{
+    .form-control {
         border: 0;
         padding: 0;
         margin: 0;
@@ -169,5 +219,4 @@ form {
         }
     }
 }
-
 </style>
